198fa3018aec00665abf807d845b041bae72aa28
rounak-yubi <rounak.bhowmick@go-yubi.com>
2023-08-02 17:13:48 +0530

    added all articles in ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'about', to: 'about#index'
-  resources :articles, only: [:show]
+  resources :articles, only: %i[show index]
   # get 'about/index'
   # get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
