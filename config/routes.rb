e9c3c948c9a43d6fedb88f363c6e7b998ea99b08
rounak-yubi <rounak.bhowmick@go-yubi.com>
2023-08-02 08:42:18 +0530

    added controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'about/index'
+  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
