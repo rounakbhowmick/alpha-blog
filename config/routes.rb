8275625dc4eeae530ad34ed785f69b1aa1f89b50
rounak-yubi <rounak.bhowmick@go-yubi.com>
2023-08-02 23:35:30 +0530

    refactor and destroy added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'about', to: 'about#index'
-  resources :articles, only: %i[show index new create edit update]
+  # resources :articles, only: %i[show index new create edit update destroy]
+  resources :articles
   # get 'about/index'
   # get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
