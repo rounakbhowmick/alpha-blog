ab290fcca37be29b7f6bff8d700b5e415a125226
rounak-yubi <rounak.bhowmick@go-yubi.com>
2023-08-06 19:27:23 +0530

    added user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
+  EMAIL_REGEX = /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 },
+                    format: { with: EMAIL_REGEX }
 end
